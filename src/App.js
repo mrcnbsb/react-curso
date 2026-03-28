b1b4d4a7017104fb9d2fd60c1801c6673850045c
mrcnbsb <marconebsb@gmail.com>
2026-03-28 11:44:01 -0300

    Aplicando CSS

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
+import Frase from './components/Frase';
 
 function App() {
 
   const nome = "Marcone"
 
   return (
-    <div className="App">          
-     <SayMyName nome="João" />
-     <SayMyName nome="Maria" />
-     <SayMyName nome="Pedro" />
-     <SayMyName nome="Ana" />
-     <SayMyName nome="Lucas" />
-     <SayMyName nome={nome} />
-     <Pessoa nome="Marcone" idade={46} profissao="Desenvolvedor" foto="https://via.placeholder.com/150" />     
+    <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase />          
+      <Frase />          
+      <SayMyName nome="João" />
+      <SayMyName nome="Maria" />
+      <SayMyName nome="Pedro" />
+      <SayMyName nome="Ana" />
+      <SayMyName nome="Lucas" />
+      <SayMyName nome={nome} />
+      <Pessoa nome="Marcone" idade={46} profissao="Desenvolvedor" foto="https://via.placeholder.com/150" />     
     </div>
   );
 }
