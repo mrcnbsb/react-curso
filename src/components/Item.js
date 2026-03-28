3e53bb6f0848c35a2748abc2376740075297342c
mrcnbsb <marconebsb@gmail.com>
2026-03-28 17:02:16 -0300

    PropTypes e defaultProps

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,22 @@
-function Item({marca}){
+import PropTypes from 'prop-types'
+
+function Item({marca, ano_lancamento}){
+    
     return(
         <>
-            <li>{marca}</li>
+            <li>{marca} - {ano_lancamento}</li>
         </>
     )
+}
+
+Item.propTypes = {
+    marca: PropTypes.string,
+    ano_lancamento: PropTypes.number,
+}
+
+Item.defaultProps = {
+    marca: 'Faltou a marca',
+    ano_lancamento: 0,
 }
 
 export default Item
--- src/components/List.js
+++ src/components/List.js
@@ -5,10 +5,11 @@ function List(){
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Ferrari" />
-                <Item marca="BMW" />
+                <Item marca="Ferrari" ano_lancamento={1985} />
+                <Item marca="BMW" ano_lancamento={1964} />
                 <Item marca="Audi" />
-                <Item marca="Mercedes" />
+                <Item ano_lancamento={2022} />
+                <Item />
             </ul>
         </>
     )
