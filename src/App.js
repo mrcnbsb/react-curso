960a922b7155b12320149d12bd708e2c088943a5
mrcnbsb <marconebsb@gmail.com>
2026-03-28 11:17:19 -0300

    Props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 function App() {
+
+  const nome = "Marcone"
+
   return (
-    <div className="App">
-      <h1>Olá React!</h1>
-      <p>Meu primeiro App.</p>
+    <div className="App">          
+     <SayMyName nome="João" />
+     <SayMyName nome="Maria" />
+     <SayMyName nome="Pedro" />
+     <SayMyName nome="Ana" />
+     <SayMyName nome="Lucas" />
+     <SayMyName nome={nome} />
+     <Pessoa nome="Marcone" idade={46} profissao="Desenvolvedor" foto="https://via.placeholder.com/150" />     
     </div>
   );
 }
---
+++ src/components/SayMyName.js
@@ -0,0 +1,7 @@
+function SayMyName(props){
+    return(
+        <div><p>Fala aí {props.nome}, blz?!</p></div>
+    )
+}
+
+export default SayMyName
