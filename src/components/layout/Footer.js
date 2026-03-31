297adaa4d424e6b6204d8df263b0322cf0b7972a
mrcnbsb <marconebsb@gmail.com>
2026-03-30 21:54:42 -0300

    React icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,17 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa6'
+import styles from './Footer.module.css'
+
 function Footer(){
-    return <footer>Rodapé</footer>
+    return <footer>
+        <ul className={styles.social_list}>
+            <li className={styles.item}><FaFacebook size={30} color="#3b5998" /></li>
+            <li className={styles.item}><FaInstagram size={30} color="#3b5998" /></li>
+            <li className={styles.item}><FaLinkedin size={30} color="#3b5998" /></li>            
+        </ul>
+        <hr />
+        <p>Nosso Rodapé</p>
+        
+    </footer>
 }
 
 export default Footer
